abb5a432f8262cbf047cba33af5fbbfbb812debb
KatherineMarschner <info@equustone.net>
2014-07-26 13:37:18 -0500

    missing files

--- gen/com/example/financefragment/R.java
+++ gen/com/example/financefragment/R.java
@@ -16,7 +16,7 @@ public final class R {
     public static final class id {
         public static final int answerField=0x7f060008;
         public static final int button1=0x7f060004;
-        public static final int linearLayout_graphEdit=0x7f060003;
+        public static final int linearLayout_editFragment=0x7f060003;
         public static final int linearLayout_graphFragment=0x7f060006;
         public static final int listView1=0x7f060005;
         public static final int questionText=0x7f060007;
